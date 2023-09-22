8aca30d3d4367498b47c2eb61b04044c40b571e2
sunghyun1999 <32184099@dankook.ac.kr>
2023-09-22 14:07:25 +0900

    스터디 그룹 생성 구현

--- src/main/java/com/group/letscoding/handler/ControllerExceptionHandler.java
+++ src/main/java/com/group/letscoding/handler/ControllerExceptionHandler.java
@@ -1,8 +1,13 @@
 package com.group.letscoding.handler;
 
 import com.group.letscoding.dto.CMRespDto;
+import com.group.letscoding.handler.ex.CustomApiException;
+import com.group.letscoding.handler.ex.CustomException;
+import com.group.letscoding.handler.ex.CustomValidationApiException;
 import com.group.letscoding.handler.ex.CustomValidationException;
 import com.group.letscoding.util.Script;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +25,20 @@ public class ControllerExceptionHandler {
         } else {
             return Script.back(e.getErrorMap().toString());
         }
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public String exception(CustomException e) {
+        return Script.back(e.getMessage());
+    }
+
+    @ExceptionHandler(CustomValidationApiException.class)
+    public ResponseEntity<?> validationApiException(CustomValidationApiException e) {
+        return new ResponseEntity<>(new CMRespDto<>(-1, e.getMessage(), e.getErrorMap()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CustomApiException.class)
+    public ResponseEntity<?> apiException(CustomApiException e) {
+        return new ResponseEntity<>(new CMRespDto<>(-1, e.getMessage(), null), HttpStatus.BAD_REQUEST);
     }
 }
